cdfda27e271f175dbb8318809f78841c85dc44b8
Spencer <spengy@gmail.com>
2013-05-14 21:04:50 -0600

    Better pi constant in Dubins

--- Dubins/dubinGuts/dubinsSolver.cpp
+++ Dubins/dubinGuts/dubinsSolver.cpp
@@ -42,7 +42,7 @@ using namespace std;
 #define INFINITY std::numeric_limits<qreal>::infinity();
 #endif
 #ifndef M_PI
-#define M_PI 3.14159265
+#define M_PI 3.14159265358979323846264338327950288
 #endif
 
 const int DIRDATA[][3] = {
