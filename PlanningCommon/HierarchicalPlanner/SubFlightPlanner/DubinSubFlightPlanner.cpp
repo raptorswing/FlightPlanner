f07849af5321206e0b93f8b77e7c33bdcfe502dc
Spencer <spengy@gmail.com>
2013-05-31 11:49:04 -0600

    Dubins sub flight now can choose different angles
    rather than just the one "center angle."

--- PlanningCommon/HierarchicalPlanner/SubFlightPlanner/DubinSubFlightPlanner.cpp
+++ PlanningCommon/HierarchicalPlanner/SubFlightPlanner/DubinSubFlightPlanner.cpp
@@ -2,6 +2,8 @@
 
 #include <QtDebug>
 
+const qreal ANGLE_INTERVAL = 3.14159265 / 8.0;
+
 DubinSubFlightPlanner::DubinSubFlightPlanner(const UAVParameters &uavParams,
                                              const QSharedPointer<FlightTask> &task,
                                              const QSharedPointer<FlightTaskArea> &area,
@@ -15,63 +17,81 @@ bool DubinSubFlightPlanner::plan()
 {
     this->setResults(Wayset());
 
-    const QList<Position> bins = this->task()->bins(this->area()->geoPoly());
-    const QList<Position> tBins = this->task()->transformedBins(this->area()->geoPoly());
+    QList<Position> bins = this->task()->bins(this->area()->geoPoly());
+    QList<Position> locations = this->task()->transformedBins(this->area()->geoPoly());
 
-    QList<Position> binsToUse = tBins;
-    if (binsToUse.isEmpty())
-        binsToUse = bins;
 
-    UAVOrientation approachAngle(this->task()->validSensorAngleRange().center().degrees() + 180.0, false);
+    //Get a list of orientations that we should try in our search
+    QList<UAVOrientation> orientations;
 
-    UAVParameters fudgeParams = this->uavParams();
-    fudgeParams.setMinTurningRadius(fudgeParams.minTurningRadius() * 1.02);
+    {
+        qreal widthRads = 2.0 * 3.14159265;
+        if (this->task()->sensorType() == FlightTask::DirectionalSensor)
+            widthRads = this->uavParams().directionalSensorViewAngleRadians();
+
+        const qreal centerAngleRads = locations.first().angleTo(bins.first());
+        const AngleRange angleRange(UAVOrientation(centerAngleRads),widthRads, true);
+
+        UAVOrientation toAdd(centerAngleRads);
+        for (int j = 0; j < (2.0 * 3.14159265) / ANGLE_INTERVAL; j++)
+        {
+            if (angleRange.withinRange(toAdd))
+                orientations.append(toAdd);
+
+            toAdd = UAVOrientation(toAdd.radians() + ANGLE_INTERVAL);
+        }
+    }
 
     Wayset current;
     current.append(this->startPos(), this->startPose());
-
     qreal bestScore = -500;
 
-    while (!binsToUse.isEmpty() && bestScore < this->task()->maxTaskPerformance())
+    while (!locations.isEmpty() && bestScore < this->task()->maxTaskPerformance())
     {
         bestScore = -500;
         int bestBinIndex = -1;
+        UAVOrientation bestOrientation;
 
-        for (int i = 0; i < binsToUse.size(); i++)
+        for (int i = 0; i < locations.size(); i++)
         {
-            const Position& binPos = binsToUse.at(i);
+            const Position& binPos = locations.at(i);
             Wayset toTest = current;
-            toTest.append(binPos, approachAngle);
 
-            //toTest = toTest.resample(3.0, fudgeParams);
-
-            const qreal score = this->task()->calculateFlightPerformance(toTest,
-                                                                         this->area()->geoPoly(),
-                                                                         this->uavParams(),
-                                                                         false);
-            if (score > bestScore)
+            foreach(const UAVOrientation& approachAngle, orientations)
             {
-                bestScore = score;
-                bestBinIndex = i;
+                toTest.append(binPos, approachAngle);
+
+                const qreal score = this->task()->calculateFlightPerformance(toTest,
+                                                                             this->area()->geoPoly(),
+                                                                             this->uavParams(),
+                                                                             false);
+                if (score > bestScore)
+                {
+                    bestScore = score;
+                    bestOrientation = approachAngle;
+                    bestBinIndex = i;
+                }
             }
         }
 
         //qDebug() << bestScore << "/" << _task->maxTaskPerformance();
 
-        if (bestBinIndex < 0 || bestBinIndex >= binsToUse.size())
+        if (bestBinIndex < 0 || bestBinIndex >= locations.size())
         {
             qWarning() << "best bin index is out of range";
             return false;
         }
 
-        const Position toAdd = binsToUse.at(bestBinIndex);
+        const Position toAdd = locations.at(bestBinIndex);
         if (this->task()->taskType() != "Sampling")
-            binsToUse.removeAt(bestBinIndex);
-        current.append(toAdd, approachAngle);
+            locations.removeAt(bestBinIndex);
+        current.append(toAdd, bestOrientation);
     }
 
     qDebug() << bestScore;
 
+    UAVParameters fudgeParams = this->uavParams();
+    fudgeParams.setMinTurningRadius(fudgeParams.minTurningRadius() * 1.02);
     this->setResults(current.resample(this->uavParams().waypointInterval(), fudgeParams));
 
     return true;
